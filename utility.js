b7d6d4728c6ccaf883e19319e14f1db2bc78d4f7
sg1301 <88100978+sg1301@users.noreply.github.com>
2021-10-21 20:46:45 +0530

    Add files via upload

---
+++ utility.js
@@ -0,0 +1,8 @@
+let utility = {}
+utility.types = {
+    media: ["mp4","mkv"],
+    archives: ["zip","'7z",'rar','tar','gz','ar','iso',"xz"],
+    documents: ['pdf','docx','doc','xlsx','odf','txt','odt','odp','ods','odg','ps'],
+    app: ['exe','dmg','pkg',"deb"] 
+}
+module.exports=utility;
